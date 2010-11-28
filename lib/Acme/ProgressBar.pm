0f462ed9d119df8c4cca0fc16f5c89dbe527c5d3
Ricardo Signes <rjbs@cpan.org>
2010-11-28 08:12:07 -0500

    dzilify

--- lib/Acme/ProgressBar.pm
+++ lib/Acme/ProgressBar.pm
@@ -1,21 +1,9 @@
-package Acme::ProgressBar;
-
 use strict;
 use warnings;
+package Acme::ProgressBar;
+# ABSTRACT: a simple progress bar for the patient
 
 use Time::HiRes ();
-
-=head1 NAME 
-
-Acme::ProgressBar - a simple progress bar for the patient
-
-=head1 VERSION
-
-version 1.125
-
-=cut
-
-our $VERSION = '1.125';
 
 =head1 SYNOPSIS
 
@@ -26,6 +14,26 @@ our $VERSION = '1.125';
 
 use base qw(Exporter);
 our @EXPORT = qw(progress); ## no critic Export
+
+=head1 DESCRIPTION
+
+Acme::ProgressBar provides a simple solution designed to provide accurate
+countdowns.  No progress bar object needs to be created, and all the
+calculation of progress through total time required is handled by the module
+itself.
+
+=func progress
+
+ progress { unlink $_ for <*> };
+ progress { while (<>) { $ua->get($_) } };
+ progress { sleep 5; }
+
+There is only one function exported by default, C<progress>.  This function
+takes a coderef as its lone argument.  It will execute this code and display a
+simple progress bar indicating the time required for ten iterations through the
+code.
+
+=cut
 
 sub progress(&) { ## no critic Prototype
 	my ($code) = @_;
@@ -63,49 +71,11 @@ sub _overprint {
 	print $message, "\r";
 }
 
-"48102931829 minutes remaining";
-
-__END__
-
-
-=head1 DESCRIPTION
-
-Acme::ProgressBar provides a simple solution designed to provide accurate
-countdowns.  No progress bar object needs to be created, and all the
-calculation of progress through total time required is handled by the module
-itself.
-
-=head1 FUNCTIONS
-
-=head2 C<< progress >>
-
- progress { unlink $_ for <*> };
- progress { while (<>) { $ua->get($_) } };
- progress { sleep 5; }
-
-There is only one function exported by default, C<progress>.  This function
-takes a coderef as its lone argument.  It will execute this code and display a
-simple progress bar indicating the time required for ten iterations through the
-code.
-
 =head1 TODO
 
-=over
+=for :list
+* allow other divisions of time (other than ten)
 
-=item *
+=cut
 
-allow other divisions of time (other than ten)
-
-=back
-
-=head1 AUTHOR
-
-Ricardo SIGNES E<lt>rjbs@cpan.orgE<gt>
-
-Thanks to Steve Lidie for pointing out a stupid error in 1.001: I couldn't
-count to ten, and he could.
-
-=head1 COPYRIGHT
-
-Copyright (C) 2004, Ricardo SIGNES.  Available under the same terms as Perl
-itself.
+"48102931829 minutes remaining";
--- t/00.load.t
+++
@@ -1,7 +0,0 @@
-#!perl -T
-
-use Test::More tests => 1;
-
-use_ok( 'Acme::ProgressBar' );
-
-diag( "Testing Acme::ProgressBar $Acme::ProgressBar::VERSION" );
